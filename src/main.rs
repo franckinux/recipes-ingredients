e120ccea9e1c6ef0893471b53c0675b6a776beec
Franck Barbenoire <contact@franck-barbenoire.fr>
2021-12-05 15:08:35 +0100

    add a command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-use std::env;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-// use crate::yaml_utils::load_file;
+use crate::yaml_utils::load_file;
 
 mod yaml_utils;
 
 
-fn main() {
-    let args: Vec<_> = env::args().collect();
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    /// Order files
+    #[structopt(short, long, parse(from_os_str), default_value="data/prix-de-revient.yaml")]
+    orders: Vec<PathBuf>,
 
-    yaml_utils::load_file(&args[1]);
+    /// Html file
+    #[structopt(short, long, parse(from_os_str))]
+    html: Option<PathBuf>,
+
+    /// Ingredients file
+    #[structopt(short, long, parse(from_os_str), default_value="data/matieres-premieres/matieres-premieres.yaml")]
+    ingredients: PathBuf,
+}
+
+
+fn main() {
+    // let args: Vec<_> = env::args().collect();
+    let opt = Opt::from_args();
+
+    let ingredients = opt.ingredients.to_str().unwrap();
+
+    println!("{:?}", ingredients);
+    yaml_utils::load_file(ingredients);
 }
